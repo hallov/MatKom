bc1ede44cc6d9d6e7af65ab53165368a7113d8e6
4lfman <31034983+4lfman@users.noreply.github.com>
2023-02-14 12:52:04 +0100

    La till lite kommentarer men fattar fortfarande inte varför vi inte får mer diffusion från kanterna och måste sätta extrema värden för att något ska hända. Ligger det i att diffusionen spelar så liten roll och advection har större effekt och den beräknas inte i boudry

--- DiffAdvReact1_2.m
+++ DiffAdvReact1_2.m
@@ -4,7 +4,7 @@ L = 1;
 Tfinal = 100;
 J = 100;
 
-dx = L/(J + 1);
+dx = L/(J + 1); 
 dt = 1e-3/4;
 
 t = 1;
@@ -13,9 +13,11 @@ xx = linspace(0,L,J + 2).';
 
 %%%%%%%%%% PHYSICAL PARAMETERS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 d = 1/10;
-v = 3;
-f = @(t,u) 0*u; %% Ingen nytt ?
-r =@(c) 0; %% negativ reaction ger mer c??
+v = 2;
+f = @(t,u) 0*u; %% Ingen nytt ? %%Vi skickar in c så vad skiljer f från r?
+r =@(c) 0; %% negativ reaction ger mer c?? %%Vid positiv r går partiklarna in i reaktioner och 
+% försvinner (tolkningsfråga, annars ändra tecken i uppdateringen där nere för omvänd tolkning)
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%%%%%%%%% INITIAL CONDITION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -33,7 +35,7 @@ end
 %%%%%%%%%% CONSTRUCTING FINITE DIFFERENCES %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 A = full(spdiags(bsxfun(@times,ones(J,1),[-1 0 1]),[-1 0 1],J,J))/(2*dx);
 C=zeros(J-2,1);
-B=[100;C;0]; %%Tror har med ska hjälpa med boundires
+B=[1;C;0]; %%Tror har med ska hjälpa med boundires
 D = (full(spdiags(bsxfun(@times,ones(J,1),[1 -2 1]),[-1 0 1],J,J)))/(dx*dx);
 
 M = d*D - v*A;
@@ -41,7 +43,7 @@ M = d*D - v*A;
 
 %%%%%%%%%% PREPARING PLOT %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 ax = axes;
-p = plot(xx,[100;c;0],'.-');
+p = plot(xx,[1;c;0],'.-'); %%Gränsvärdena här ändras direkt vid första beräkningen men måste finnas för att fylla grafen?
 p.DisplayName = "Pollutant concentration";
 ax.YLimMode = 'manual';
 xlabel('m')
@@ -54,7 +56,7 @@ legend show
 %%%%%%%%%% SOLVING THE PROBLEM %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 while t < Tfinal
     c = c + dt*(M*c+B - r(c) + f(t,c));
-    p.YData = [100;c;0]; %% tvingar kanterna är 0 tror inte den som fuckar
+    p.YData = [1;c;0]; %% tvingar kanterna är 0 tror inte den som fuckar %%Tror inte detta påverka beräkningarna utan bara ploten
     t = t + dt;
     ax.Title.String = sprintf("t = %.3e",t);
     drawnow
